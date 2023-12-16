5ac36690db0fb859618877c2bd7a873d2dae9a22
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2023-12-16 13:19:26 +0600

    create user services

---
+++ src/services/user.services.ts
@@ -0,0 +1,49 @@
+import { IUser } from '../interfaces/users.interface';
+import User from '../models/users.model';
+
+// create a new user unto mongoDB
+const createUserIntoDB = async (userData: IUser): Promise<IUser> => {
+  const result = await User.create(userData);
+  return result;
+};
+
+// get all users from mongoDB
+const getAllUsersFromDB = async (): Promise<IUser[]> => {
+  const result = await User.find();
+  return result;
+};
+
+// get specific user from mongoDB
+const getSingleUserFrom = async (id: string): Promise<IUser | null> => {
+  const result = User.findById(id);
+  return result;
+};
+
+// update an user based on user id
+const updateUserFromDB = async (
+  id: string,
+  userData: IUser,
+): Promise<IUser | null> => {
+  const result = User.findByIdAndUpdate(id, userData, {
+    new: true, // Return the updated document instead of the original
+    runValidators: true, // Run any specified validation on the update operation
+  });
+  return result;
+};
+
+// delete an user from DB
+const deleteUserFromDB = async (
+  id: string,
+  userData: IUser,
+): Promise<IUser | null> => {
+  const result = await User.findByIdAndDelete(id, userData);
+  return result;
+};
+
+export const userServices = {
+  createUserIntoDB,
+  getAllUsersFromDB,
+  getSingleUserFrom,
+  updateUserFromDB,
+  deleteUserFromDB,
+};
